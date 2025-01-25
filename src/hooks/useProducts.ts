acd69f1fce879e67dc03108fcc12f6f4291c651b
Daniel <danielvillalobosfdz@gmail.com>
2025-01-25 13:46:09 +0100

    Add loading and error states

--- src/hooks/useProducts.ts
+++ src/hooks/useProducts.ts
@@ -4,19 +4,27 @@ import { Product } from '../types/product'
 
 export const useProducts = () => {
   const [products, setProducts] = useState<Product[]>([])
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState(false)
 
   useEffect(() => {
+    setLoading(true)
+    setError(false)
+
     const fetchProducts = async () => {
       try {
         const productsFetched = await getProducts()
         setProducts(productsFetched)
       } catch (e) {
         console.error(e)
+        setError(true)
+      } finally {
+        setLoading(false)
       }
     }
 
     fetchProducts()
   }, [])
 
-  return { products }
+  return { products, loading, error }
 }
