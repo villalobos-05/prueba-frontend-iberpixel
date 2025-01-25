31d17c1dabbbd5ffc889dc8b62bf9182a6e89d63
Daniel <danielvillalobosfdz@gmail.com>
2025-01-25 13:51:10 +0100

    Show loading and error states

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,26 @@ import ProductsGrid from './components/ProductsGrid'
 import { useProducts } from './hooks/useProducts'
 
 function App() {
-  const { products } = useProducts()
+  const { products, loading, error } = useProducts()
+
+  if (error) {
+    return (
+      <main className='text-3xl'>
+        <p>¡Error al buscar los productos! Por favor, inténtelo de nuevo.</p>
+        <button onClick={() => window.location.reload()}>
+          Prueba otra vez
+        </button>
+      </main>
+    )
+  }
 
   return (
     <main>
-      <ProductsGrid products={products} />
+      {loading ? (
+        <h2 className='text-center text-3xl font-semibold'>Cargando...</h2>
+      ) : (
+        <ProductsGrid products={products} />
+      )}
     </main>
   )
 }
