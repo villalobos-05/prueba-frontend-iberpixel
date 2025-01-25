494ad5a9fae480db3ac0ab64d314b0be4dbd1f0b
Daniel <danielvillalobosfdz@gmail.com>
2025-01-25 19:58:31 +0100

    Add theme to localstorage

--- src/components/ThemeSwitch.tsx
+++ src/components/ThemeSwitch.tsx
@@ -5,10 +5,16 @@ import { IoMoon, IoSunny } from 'react-icons/io5'
 type ThemeTypes = 'light' | 'dark'
 
 export default function ThemeSwitch() {
-  const [theme, setTheme] = useState<ThemeTypes>('light')
+  const [theme, setTheme] = useState<ThemeTypes>(
+    (localStorage.getItem('theme') as ThemeTypes) ||
+      (window.matchMedia('(prefers-color-scheme: dark)').matches
+        ? 'dark'
+        : 'light')
+  )
 
   useEffect(() => {
     document.body.className = theme
+    localStorage.setItem('theme', theme)
   }, [theme])
 
   const toggleTheme = () => {
