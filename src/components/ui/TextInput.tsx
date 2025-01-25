f574c13a3dea65bcf4869d8e3eaf94f82552f55b
Daniel <danielvillalobosfdz@gmail.com>
2025-01-25 17:10:43 +0100

    Create custom text input component

---
+++ src/components/ui/TextInput.tsx
@@ -0,0 +1,13 @@
+type Props = Omit<React.InputHTMLAttributes<HTMLInputElement>, 'type'>
+
+const TextInput = ({ className, ...props }: Props) => {
+  return (
+    <input
+      {...props}
+      type='text'
+      className={`bg-secondary focus:bg-secondary/50 rounded-lg px-2 py-1 text-lg outline-0 ${className}`}
+    />
+  )
+}
+
+export default TextInput
