83a312f86bd9832f35e6babbeb139d3be1b99f20
Daniel <danielvillalobosfdz@gmail.com>
2025-01-25 13:20:30 +0100

    Create product type

---
+++ src/types/product.d.ts
@@ -0,0 +1,25 @@
+export interface Product {
+  id: number
+  title: string
+  price: number
+  description: string
+  category: Category
+  image: string
+  rating: Rating
+}
+
+interface Rating {
+  rate: number
+  count: number
+}
+
+// '& {}' ensures that default categories don't overlap with string,
+// so Category is either one of productCategories or any other category (string)
+type Category = (typeof productCategories)[number] | (string & {})
+
+export const productCategories = [
+  "women's clothing",
+  "men's clothing",
+  'electronics',
+  'jewelery',
+] as const
