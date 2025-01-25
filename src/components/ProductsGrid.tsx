71a7a134e593333aefad98a44943ed076d6a1ceb
Daniel <danielvillalobosfdz@gmail.com>
2025-01-25 15:57:00 +0100

    Create ProductsHeader component

--- src/components/ProductsGrid.tsx
+++ src/components/ProductsGrid.tsx
@@ -1,19 +1,34 @@
 import { useProductFilters } from '../hooks/useProductFilters'
 import { Product } from '../types/product.d'
 import ProductCard from './ProductCard'
+import ProductsHeader from './ProductsHeader'
 
 export default function ProductsGrid({ products }: { products: Product[] }) {
-  const { filteredProducts, setProductName } = useProductFilters(products)
+  const { filteredProducts, setProductCategories, setProductName } =
+    useProductFilters(products)
+
+  const handleCheckbox = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { value, checked } = event.target
+
+    if (checked) {
+      setProductCategories((prevProductCategories) => [
+        ...prevProductCategories,
+        value,
+      ])
+    } else {
+      setProductCategories((prevProductCategories) =>
+        [...prevProductCategories].filter((category) => category !== value)
+      )
+    }
+  }
 
   return (
     <>
-      <div className='flex items-center justify-between gap-6'>
-        <input
-          type='text'
-          placeholder='Buscar producto'
-          onChange={(event) => setProductName(event.target.value)}
-        />
-      </div>
+      <ProductsHeader
+        onTextInputChange={(event) => setProductName(event.target.value)}
+        onCheckboxInputChange={handleCheckbox}
+      />
+
       <div className='grid grid-cols-1 gap-14 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5'>
         {filteredProducts.map((product) => (
           <ProductCard product={product} />
---
+++ src/components/ProductsHeader.tsx
@@ -0,0 +1,34 @@
+import { productCategories } from '../types/product.d'
+
+type Props = {
+  onTextInputChange: React.ChangeEventHandler<HTMLInputElement>
+  onCheckboxInputChange: React.ChangeEventHandler<HTMLInputElement>
+}
+
+export default function ProductsHeader({
+  onTextInputChange,
+  onCheckboxInputChange,
+}: Props) {
+  return (
+    <div className='flex items-center justify-between gap-6'>
+      <input
+        type='text'
+        placeholder='Buscar producto'
+        onChange={onTextInputChange}
+      />
+
+      <div className='flex gap-6'>
+        {productCategories.map((category) => (
+          <label key={category}>
+            <input
+              type='checkbox'
+              value={category}
+              onChange={onCheckboxInputChange}
+            />
+            {category}
+          </label>
+        ))}
+      </div>
+    </div>
+  )
+}
