d0d31980eacd32723f9e088976413584c5227733
Daniel <danielvillalobosfdz@gmail.com>
2025-01-26 01:40:04 +0100

    Provide cart to App

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { CartProvider } from './contexts/cart.tsx'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+  <CartProvider>
     <App />
-  </StrictMode>
+  </CartProvider>
 )
