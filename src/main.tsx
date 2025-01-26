190322f903f6099ee63199f8f59e96e1fda8d467
Daniel <danielvillalobosfdz@gmail.com>
2025-01-26 20:14:49 +0100

    Add Header component

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { CartProvider } from './contexts/cart.tsx'
+import Header from './components/Header/Header.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <CartProvider>
+    <Header />
     <App />
   </CartProvider>
 )
