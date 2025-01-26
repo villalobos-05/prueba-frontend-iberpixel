9d58e47f980a07b9386f93b66209dbb28920492a
Daniel <danielvillalobosfdz@gmail.com>
2025-01-26 21:29:04 +0100

    Refetch products in button event

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,13 @@ import { ProductFiltersProvider } from './contexts/productFitlers'
 import { useProducts } from './hooks/useProducts'
 
 function App() {
-  const { products, loading, error } = useProducts()
+  const { products, loading, error, fetchProducts } = useProducts()
 
   if (error) {
     return (
-      <main className='text-3xl'>
+      <main className='flex flex-col items-center gap-4 text-3xl'>
         <p>¡Error al buscar los productos! Por favor, inténtelo de nuevo.</p>
-        <Button onClick={() => window.location.reload()}>
+        <Button onClick={fetchProducts} className='bg-accent w-fit'>
           Prueba otra vez
         </Button>
       </main>
