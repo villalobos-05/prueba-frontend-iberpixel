1da63391421ffd698e5328601fbe58b8a715dfc4
Daniel <danielvillalobosfdz@gmail.com>
2025-01-27 10:07:57 +0100

    Display error message when no products

--- src/components/Products/Products.tsx
+++ src/components/Products/Products.tsx
@@ -1,4 +1,5 @@
 import { useProductFilters } from '../../hooks/useProductFilters'
+import ErrorMessage from '../ui/ErrorMessage'
 import ProductCard from './ProductCard'
 import ProductsFilters from './ProductsFilters'
 
@@ -9,11 +10,15 @@ export default function Products() {
     <div className='flex flex-col gap-10'>
       <ProductsFilters />
 
-      <main className='grid grid-cols-1 place-items-center gap-12 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5'>
-        {filteredProducts.map((product) => (
-          <ProductCard key={product.id} product={product} />
-        ))}
-      </main>
+      {filteredProducts.length === 0 ? (
+        <ErrorMessage>Ningún producto encontrado.</ErrorMessage>
+      ) : (
+        <main className='grid grid-cols-1 place-items-center gap-12 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5'>
+          {filteredProducts.map((product) => (
+            <ProductCard key={product.id} product={product} />
+          ))}
+        </main>
+      )}
     </div>
   )
 }
